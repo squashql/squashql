d601d01a7e5c5c3b4eb6a945e16378bec79edbda
bares.paul@gmail.com <bares.paul@gmail.com>
2022-01-14 15:14:20 +0400

    Procfile

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java -Dserver.port=$PORT $JAVA_OPTS -jar target/aitm-server-0.1-SNAPSHOT.jar
--- core/src/main/java/me/paulbares/query/SqlUtils.java
+++ core/src/main/java/me/paulbares/query/SqlUtils.java
@@ -1,7 +1,7 @@
 package me.paulbares.query;
 
 public class SqlUtils {
-  
+
   public static String escape(String column) {
     return "`" + column + "`";
   }
