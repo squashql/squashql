0a04a4c59a8cae3bf5462aaa44dca6507d16a827
bares.paul@gmail.com <bares.paul@gmail.com>
2022-01-18 16:52:58 +0400

    refactor spark query engine

---
+++ core/src/main/java/me/paulbares/query/AQueryEngine.java
@@ -0,0 +1,57 @@
+package me.paulbares.query;
+
+import me.paulbares.query.context.Totals;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public abstract class AQueryEngine implements QueryEngine {
+
+  protected abstract Table retrieveAggregates(Query query);
+
+  @Override
+  public Table execute(Query query) {
+    Table aggregates = retrieveAggregates(query);
+    return postProcessDataset(aggregates, query);
+  }
+
+  protected Table postProcessDataset(Table initialTable, Query query) {
+    if (!query.context.containsKey(Totals.KEY)) {
+      return initialTable;
+    }
+
+    return editTotalsAndSubtotals(initialTable, query);
+  }
+
+  protected Table editTotalsAndSubtotals(Table dataset, Query query) {
+    Iterator<List<Object>> rowIterator = dataset.iterator();
+
+    List<List<Object>> newRows = new ArrayList<>((int) dataset.count());
+    List<String> headers = new ArrayList<>(query.coordinates.keySet());
+    while (rowIterator.hasNext()) {
+      List<Object> objects = new ArrayList<>(rowIterator.next());
+
+      Object[] newHeaders = new String[headers.size()];
+      for (int i = 0; i < headers.size(); i++) {
+        Object current = objects.get(i);
+        if (i == 0 && current == null) {
+          // GT
+          newHeaders[i] = GRAND_TOTAL;
+        } else if (i >= 1 && objects.get(i - 1) != null && current == null) {
+          // Total
+          newHeaders[i] = TOTAL;
+        } else {
+          newHeaders[i] = current; // nothing to change
+        }
+      }
+
+      for (int i = 0; i < newHeaders.length; i++) {
+        objects.set(i, newHeaders[i]);
+      }
+      newRows.add(objects);
+    }
+
+    return new ArrayTable(dataset.fields(), newRows);
+  }
+}
--- spark/src/main/java/me/paulbares/query/SparkQueryEngine.java
+++ spark/src/main/java/me/paulbares/query/SparkQueryEngine.java
@@ -1,18 +1,12 @@
 package me.paulbares.query;
 
 import me.paulbares.SparkDatastore;
-import me.paulbares.query.context.Totals;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
-import org.apache.spark.sql.RowFactory;
-import scala.jdk.javaapi.CollectionConverters;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
 import java.util.logging.Logger;
 
-public class SparkQueryEngine implements QueryEngine {
+public class SparkQueryEngine extends AQueryEngine {
 
   private static final Logger LOGGER = Logger.getLogger(SparkQueryEngine.class.getName());
 
@@ -23,58 +17,12 @@ public class SparkQueryEngine implements QueryEngine {
   }
 
   @Override
-  public Table execute(Query query) {
-    return new DatasetTable(executeSpark(query));
-  }
-
-  public Dataset<Row> executeSpark(Query query) {
+  protected Table retrieveAggregates(Query query) {
     LOGGER.info("Executing " + query);
     String sql = SQLTranslator.translate(query);
     LOGGER.info("Translated query #" + query.id + " to " + sql);
     this.datastore.get().createOrReplaceTempView(SparkDatastore.BASE_STORE_NAME);
     Dataset<Row> ds = this.datastore.spark.sql(sql);
-    return postProcessDataset(ds, query);
-  }
-
-  protected Dataset<Row> postProcessDataset(Dataset<Row> dataset, Query query) {
-    if (!query.context.containsKey(Totals.KEY)) {
-      return dataset;
-    }
-
-    return editTotalsAndSubtotals(dataset, query);
-  }
-
-  // TODO should be put in core
-  protected Dataset<Row> editTotalsAndSubtotals(Dataset<Row> dataset, Query query) {
-    Iterator<Row> rowIterator = dataset.toLocalIterator();
-
-    List<Row> newRows = new ArrayList<>((int) dataset.count());
-    List<String> headers = new ArrayList<>(query.coordinates.keySet());
-    while (rowIterator.hasNext()) {
-      Row row = rowIterator.next();
-      List<Object> objects = new ArrayList<>(CollectionConverters.asJava(row.toSeq()));
-
-      Object[] newHeaders = new String[headers.size()];
-      for (int i = 0; i < headers.size(); i++) {
-        Object current = objects.get(i);
-        if (i == 0 && current == null) {
-          // GT
-          newHeaders[i] = GRAND_TOTAL;
-        } else if (i >= 1 && objects.get(i - 1) != null && current == null) {
-          // Total
-          newHeaders[i] = TOTAL;
-        } else {
-          newHeaders[i] = current; // nothing to change
-        }
-      }
-
-      for (int i = 0; i < newHeaders.length; i++) {
-          objects.set(i, newHeaders[i]);
-      }
-      Row newRow = RowFactory.create(objects.toArray(new Object[0]));
-      newRows.add(newRow);
-    }
-
-    return this.datastore.spark.createDataFrame(newRows, dataset.schema());
+    return new DatasetTable(ds);
   }
 }
