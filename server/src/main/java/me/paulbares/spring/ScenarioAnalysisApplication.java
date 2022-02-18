7d05be57a948acf5d716cd5061e1a6917724a51b
bares.paul@gmail.com <bares.paul@gmail.com>
2022-02-18 14:31:46 +0400

    add cors

--- server/src/main/java/me/paulbares/spring/ScenarioAnalysisApplication.java
+++ server/src/main/java/me/paulbares/spring/ScenarioAnalysisApplication.java
@@ -9,6 +9,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ScenarioAnalysisApplication {
@@ -32,5 +34,15 @@ public class ScenarioAnalysisApplication {
   @Bean
   public MappingJackson2HttpMessageConverter jackson2HttpMessageConverter() {
     return new MappingJackson2HttpMessageConverter(JacksonUtil.mapper);
+  }
+
+  @Bean
+  public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("*").allowedMethods("*");
+      }
+    };
   }
 }
