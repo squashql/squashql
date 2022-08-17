dbe8087e4f43f856f12fe53bf8e0c58e1de1dc40
paul <bares.paul@gmail.com>
2022-08-17 15:47:48 +0400

    remove dead code

--- core/src/main/java/me/paulbares/query/monitoring/QueryWatch.java
+++ core/src/main/java/me/paulbares/query/monitoring/QueryWatch.java
@@ -5,14 +5,9 @@ import lombok.ToString;
 import me.paulbares.query.Measure;
 
 import java.util.HashMap;
-import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
-import java.util.function.Function;
-import java.util.stream.Collectors;
-
-import static java.util.function.Function.*;
 
 @ToString
 public class QueryWatch {
@@ -48,23 +43,6 @@ public class QueryWatch {
     this.stopwatchByMeasure.get(measure).stop();
   }
 
-  public String toJson() {
-    Map<String, Stopwatch> children = new LinkedHashMap<>();
-    children.put(PREPARE_PLAN, this.stopwatches.get(PREPARE_PLAN));
-    children.put(PREFETCH, this.stopwatches.get(PREFETCH));
-    children.put(BUCKET, this.stopwatches.get(BUCKET));
-    children.put(EXECUTE_PLAN, this.stopwatches.get(EXECUTE_PLAN));
-    children.put(ORDER, this.stopwatches.get(ORDER));
-
-    StringBuilder sb = new StringBuilder();
-    addParentAndChildren(this,
-            sb,
-            this.stopwatches.get(GLOBAL),
-            children,
-            identity());
-    return sb.toString();
-  }
-
   public QueryTimings toQueryTimings() {
     QueryTimings queryTimings = new QueryTimings();
     TimeUnit unit = TimeUnit.MICROSECONDS;
@@ -87,65 +65,5 @@ public class QueryWatch {
     }
 
     return queryTimings;
-  }
-
-  private static <T> void addParentAndChildren(
-          QueryWatch queryWatch,
-          StringBuilder sb,
-          Stopwatch parent,
-          Map<T, Stopwatch> children,
-          Function<T, String> keyPrinter) {
-    sb.append('{'); // start obj
-    addFieldWithColon(sb, "total");
-    addField(sb, parent.toString());
-    sb.append(',');
-    addFieldWithColon(sb, "detail");
-    sb.append('{'); // start obj detail
-
-    var it = children.entrySet().iterator();
-    while (it.hasNext()) {
-      var e = it.next();
-      T stopwatchKey = e.getKey();
-      if (stopwatchKey.equals(PREPARE_PLAN)) {
-        addFieldWithColon(sb, PREPARE_PLAN);
-        Map<String, Stopwatch> collect = queryWatch.stopwatches.entrySet().stream()
-                .filter(ee -> PREPARE_CHILDREN.contains(ee.getKey()))
-                .collect(Collectors.toMap(ee -> ee.getKey(), ee -> ee.getValue()));
-        addParentAndChildren(
-                queryWatch,
-                sb,
-                e.getValue(),
-                collect,
-                identity());
-      } else if (stopwatchKey.equals(EXECUTE_PLAN)) {
-        addFieldWithColon(sb, EXECUTE_PLAN);
-        addParentAndChildren(
-                queryWatch,
-                sb,
-                e.getValue(),
-                queryWatch.stopwatchByMeasure,
-                m -> {
-                  String alias = m.alias();
-                  return alias == null ? m.expression() : alias;
-                });
-      } else {
-        addFieldWithColon(sb, keyPrinter.apply(stopwatchKey));
-        addField(sb, e.getValue().toString());
-      }
-      if (it.hasNext()) {
-        sb.append(',');
-      }
-    }
-
-    sb.append('}'); // end obj detail
-    sb.append('}'); // end obj
-  }
-
-  public static void addField(StringBuilder sb, String field) {
-    sb.append('"').append(field).append('"');
-  }
-
-  public static void addFieldWithColon(StringBuilder sb, String field) {
-    sb.append('"').append(field).append('"').append(':');
   }
 }
