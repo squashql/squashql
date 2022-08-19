2c7cbc41c9b762c696039bd9e5d335b297aa76bb
paul <bares.paul@gmail.com>
2022-08-19 10:47:28 +0400

    save

--- server/src/test/java/me/paulbares/js/TestJavascriptLibrary.java
+++ server/src/test/java/me/paulbares/js/TestJavascriptLibrary.java
@@ -1,15 +1,21 @@
 package me.paulbares.js;
 
+import me.paulbares.jackson.JacksonUtil;
+import me.paulbares.query.dto.QueryDto;
+import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
-import java.net.URL;
+import java.io.IOException;
 
 public class TestJavascriptLibrary {
 
   @Test
-  void testReadJson() {
-    URL url = this.getClass().getResource("../../");
-    File myObj = new File("filename.txt");
+  void testReadJson() throws IOException {
+    String name = "query.json";
+    ClassLoader classLoader = getClass().getClassLoader();
+    File file = new File(classLoader.getResource(name).getFile());
+    String data = FileUtils.readFileToString(file, "UTF-8");
+    JacksonUtil.deserialize(data, QueryDto.class);// we do not test anything yet, simply that it does not throw.
   }
 }
