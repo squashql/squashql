c8d7270de09723c1a197d7302128e337c43fecd9
paul <bares.paul@gmail.com>
2022-09-21 09:49:21 +0400

    add x-api-key

--- java-client/src/main/java/me/paulbares/client/http/HttpClientQuerier.java
+++ java-client/src/main/java/me/paulbares/client/http/HttpClientQuerier.java
@@ -1,8 +1,6 @@
 package me.paulbares.client.http;
 
-import feign.Feign;
-import feign.Headers;
-import feign.RequestLine;
+import feign.*;
 import feign.jackson.JacksonDecoder;
 import feign.jackson.JacksonEncoder;
 import me.paulbares.jackson.JacksonUtil;
@@ -19,6 +17,7 @@ public class HttpClientQuerier {
   private static final OkHttpClient client = new OkHttpClient();
 
   private static final Feign.Builder builder = Feign.builder()
+          .requestInterceptor(template -> template.header("x-api-key", "c3981f51-e9e8-47e1-aa81-7c26e05c98d9"))
           .client(new feign.okhttp.OkHttpClient(client))
           .encoder(new JacksonEncoder(JacksonUtil.mapper))
           .decoder(new JacksonDecoder(JacksonUtil.mapper));
