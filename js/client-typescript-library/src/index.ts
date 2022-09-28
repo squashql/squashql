d7779dca0197f39100401ae520534da2611c97bc
paul <bares.paul@gmail.com>
2022-09-28 16:38:51 +0400

    test client js

--- js/client-typescript-library/src/index.ts
+++ js/client-typescript-library/src/index.ts
@@ -1,19 +1,4 @@
-import {
-  BucketColumnSet,
-  ColumnSetKey,
-  ComparisonMeasure,
-  ComparisonMethod,
-  divide,
-  eq,
-  PeriodColumnSet,
-  plus,
-  Querier,
-  Query,
-  sum,
-  sumIf,
-  Table,
-  Year
-} from "aitm-js-query"
+import {ComparisonMethod, eq, ParentComparisonMeasure, Querier, Query, sum, sumIf, Table} from "aitm-js-query"
 
 const querier = new Querier("http://localhost:8080");
 const assets = "https://raw.githubusercontent.com/paulbares/aitm-assets/main/metrics.json";
@@ -32,35 +17,40 @@ const q = new Query()
         .withColumn("scenario encrypted")
 
 const amount = sum("amount.sum", "Amount");
-const sales = sumIf("sales", "Amount", "Income/Expense", eq("Revenue"));
-const ebidtaRatio = divide("EBIDTA %", amount, sales);
+// const sales = sumIf("sales", "Amount", "Income/Expense", eq("Revenue"));
 
 q.withMeasure(amount)
-q.withMeasure(sales)
-q.withMeasure(ebidtaRatio)
+// q.withMeasure(sales)
 
-const refPeriod = {"Year": "y-1"}
-const growth = new ComparisonMeasure("Growth", ComparisonMethod.DIVIDE, sales, ColumnSetKey.PERIOD, new Map(Object.entries(refPeriod)));
-q.withMeasure(growth)
-q.withPeriodColumnSet(new PeriodColumnSet(new Year("Year")))
+// const pop = new ParentComparisonMeasure("percentOfParent", ComparisonMethod.DIVIDE, sales, ["Month", "Year"]);
+// q.withMeasure(pop)
 
-const kpi = plus("KPI", ebidtaRatio, growth);
-q.withMeasure(kpi)
+// const ebidtaRatio = divide("EBIDTA %", amount, sales);
+// q.withMeasure(ebidtaRatio)
 
-const refScenario = {"scenario encrypted": "s-1", "group": "g"}
-const kpiComp = new ComparisonMeasure("KPI comp. with prev. scenario",
-        ComparisonMethod.ABSOLUTE_DIFFERENCE,
-        kpi,
-        ColumnSetKey.BUCKET,
-        new Map(Object.entries(refScenario)));
-q.withMeasure(kpiComp);
-const groups = {
-  "group1": ["A", "B", "C", "D"],
-  "group2": ["A", "D"],
-}
-q.withBucketColumnSet(new BucketColumnSet("group", "scenario encrypted", new Map(Object.entries(groups))));
+// const refPeriod = {"Year": "y-1"}
+// const growth = new ComparisonMeasureReferencePosition("Growth", ComparisonMethod.DIVIDE, sales, ColumnSetKey.PERIOD, new Map(Object.entries(refPeriod)));
+// q.withMeasure(growth)
+// q.withPeriodColumnSet(new PeriodColumnSet(new Year("Year")))
 
-querier.execute(q).then(r => {
-  console.log(`Metadata result: ${toString(r.metadata)}`);
-  console.log(`Table: ${toString(r.table)}`);
+// const kpi = plus("KPI", ebidtaRatio, growth);
+// q.withMeasure(kpi)
+
+// const refScenario = {"scenario encrypted": "s-1", "group": "g"}
+// const kpiComp = new ComparisonMeasureReferencePosition("KPI comp. with prev. scenario",
+//         ComparisonMethod.ABSOLUTE_DIFFERENCE,
+//         kpi,
+//         ColumnSetKey.BUCKET,
+//         new Map(Object.entries(refScenario)));
+// q.withMeasure(kpiComp);
+// const groups = {
+//   "group1": ["A", "B", "C", "D"],
+//   "group2": ["A", "D"],
+// }
+// q.withBucketColumnSet(new BucketColumnSet("group", "scenario encrypted", new Map(Object.entries(groups))));
+
+querier.execute0(q).then(r => {
+  console.log(r);
+  // console.log(`Metadata result: ${toString(r.metadata)}`);
+  // console.log(`Table: ${toString(r.table)}`);
 })
