57984727e42e337a60cff37be6eb925edeac88e6
paul <bares.paul@gmail.com>
2022-11-17 17:03:27 +0400

    fix method

--- core/src/main/java/me/paulbares/util/AitmArrays.java
+++ core/src/main/java/me/paulbares/util/AitmArrays.java
@@ -2,7 +2,19 @@ package me.paulbares.util;
 
 public final class AitmArrays {
 
-  public static int search(int[] array, int i) {
-    return java.util.Arrays.stream(array).filter(aa -> aa == i).findFirst().orElse(-1);
+  /**
+   * Searches the specified array of ints for the specified value.
+   *
+   * @param a the array to be searched
+   * @param key the value to be searched for
+   * @return index of the search key, if it is contained in the array; otherwise, <code>- 1</code>.
+   */
+  public static int search(int[] a, int key) {
+    for (int j = 0; j < a.length; j++) {
+      if (a[j] == key) {
+        return j;
+      }
+    }
+    return -1;
   }
 }
