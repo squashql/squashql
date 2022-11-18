f406da341e2478f5182d981e5c353f2e01c3be85
paul <bares.paul@gmail.com>
2022-11-18 16:41:50 +0400

    save

--- js/client-typescript-library/src/index.ts
+++ js/client-typescript-library/src/index.ts
@@ -3,7 +3,7 @@ import {
   from,
   sum,
   Semester,
-  comparisonMeasureWithPeriod,
+  comparisonMeasureWithParent,
   Querier,
 } from "aitm-js-query"
 
@@ -12,26 +12,33 @@ const assets = "https://raw.githubusercontent.com/paulbares/aitm-assets/main/met
 
 const toString = (a: any): string => JSON.stringify(a, null, 1)
 
-querier.getMetadata(assets).then(r => {
-  console.log(`Store: ${toString(r.stores)}`);
-  console.log(`Measures: ${toString(r.measures)}`)
-  console.log(`Agg Func: ${r.aggregationFunctions}`)
-})
-
-const scoreSum = sum("score_sum", "score");
-const comparisonScore = comparisonMeasureWithPeriod(
-        "compare with previous year",
-        ComparisonMethod.ABSOLUTE_DIFFERENCE,
-        scoreSum,
-        new Map(Object.entries({"semester": "s-1", "year": "y"})),
-        new Semester("semester", "year"));
-
-const q = from("student")
-        .select(["year", "semester", "name"], [], [scoreSum, comparisonScore])
+const pop = sum("pop", "population")
+const ratio = comparisonMeasureWithParent("ratio", ComparisonMethod.DIVIDE, pop, ["city", "country", "continent"])
+const query = from("populationTable")
+        .select(["continent", "country", "city"], [], [pop, ratio])
+        .rollup(["continent", "country", "city"])
         .build();
 
-querier.execute0(q).then(r => {
-  console.log(r);
+// querier.getMetadata(assets).then(r => {
+//   console.log(`Store: ${toString(r.stores)}`);
+//   console.log(`Measures: ${toString(r.measures)}`)
+//   console.log(`Agg Func: ${r.aggregationFunctions}`)
+// })
+//
+// const scoreSum = sum("score_sum", "score");
+// const comparisonScore = comparisonMeasureWithPeriod(
+//         "compare with previous year",
+//         ComparisonMethod.ABSOLUTE_DIFFERENCE,
+//         scoreSum,
+//         new Map(Object.entries({"semester": "s-1", "year": "y"})),
+//         new Semester("semester", "year"));
+//
+// const q = from("student")
+//         .select(["year", "semester", "name"], [], [scoreSum, comparisonScore])
+//         .build();
+//
+// querier.execute0(q).then(r => {
+//   console.log(r);
   // console.log(`Metadata result: ${toString(r.metadata)}`);
   // console.log(`Table: ${toString(r.table)}`);
-})
+// })
