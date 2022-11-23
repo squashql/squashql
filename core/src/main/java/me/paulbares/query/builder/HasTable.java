2ffeda8914326792d6cdb92363a9569d70fe596d
paul <bares.paul@gmail.com>
2022-11-23 22:33:29 +0400

    deprecated

--- core/src/main/java/me/paulbares/query/builder/HasTable.java
+++ core/src/main/java/me/paulbares/query/builder/HasTable.java
@@ -5,6 +5,11 @@ import me.paulbares.query.dto.CriteriaDto;
 
 public interface HasTable extends HasCondition {
 
+  /**
+   * Kept for now not to have to rewrite a lot of tests. Yes I am lazy :). Use {@link #where(CriteriaDto)} instead.
+   */
+  @Deprecated
   HasTable where(String field, ConditionDto conditionDto);
+
   HasTable where(CriteriaDto criteriaDto);
 }
