8084ff94741b8303c2691a1bf734be337dd64ee4
paul <bares.paul@gmail.com>
2023-01-04 11:35:31 +0400

    clean

--- snowflake/src/test/java/me/paulbares/query/SnowflakeTestUtil.java
+++ snowflake/src/test/java/me/paulbares/query/SnowflakeTestUtil.java
@@ -2,12 +2,17 @@ package me.paulbares.query;
 
 public class SnowflakeTestUtil {
 
-  public static final String jdbcUrl = "jdbc:snowflake://<account_identifier>.snowflakecomputing.com";
-  public static final String username = "";
+//  public static final String jdbcUrl = "jdbc:snowflake://<account_identifier>.snowflakecomputing.com";
+  public static final String username = "Paul";
   public static final String password = "";
-  public static final String warehouse = "";
-  public static final String database = "";
-  public static final String schema = "";
+//  public static final String warehouse = "";
+//  public static final String database = "";
+//  public static final String schema = "";
+
+  public static final String jdbcUrl = "jdbc:snowflake://qu12379.north-europe.azure.snowflakecomputing.com";
+  public static final String warehouse = "COMPUTE_WH";
+  public static final String database = "TEST";
+  public static final String schema = "PUBLIC";
 
   /**
    * See {@link me.paulbares.SnowflakeUtil}.
