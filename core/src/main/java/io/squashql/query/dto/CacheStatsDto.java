ac4942fb0a9a5a907ae0c7c60faff44d7d46cd3e
paul <bares.paul@gmail.com>
2023-03-22 15:54:23 +0400

    add missing plugins and info to parent pom

--- core/src/main/java/io/squashql/query/dto/CacheStatsDto.java
+++ core/src/main/java/io/squashql/query/dto/CacheStatsDto.java
@@ -6,7 +6,6 @@ import lombok.*;
 @EqualsAndHashCode
 @NoArgsConstructor // For Jackson
 @Getter
-@Builder
 public class CacheStatsDto {
   public long hitCount;
   public long missCount;
@@ -16,5 +15,37 @@ public class CacheStatsDto {
     this.hitCount = hitCount;
     this.missCount = missCount;
     this.evictionCount = evictionCount;
+  }
+
+  public static CacheStatsDtoBuilder builder() {
+    return new CacheStatsDtoBuilder();
+  }
+
+  public static class CacheStatsDtoBuilder {
+    private long hitCount;
+    private long missCount;
+    private long evictionCount;
+
+    CacheStatsDtoBuilder() {
+    }
+
+    public CacheStatsDtoBuilder hitCount(long hitCount) {
+      this.hitCount = hitCount;
+      return this;
+    }
+
+    public CacheStatsDtoBuilder missCount(long missCount) {
+      this.missCount = missCount;
+      return this;
+    }
+
+    public CacheStatsDtoBuilder evictionCount(long evictionCount) {
+      this.evictionCount = evictionCount;
+      return this;
+    }
+
+    public CacheStatsDto build() {
+      return new CacheStatsDto(this.hitCount, this.missCount, this.evictionCount);
+    }
   }
 }
