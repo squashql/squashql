312be0481b426500059ef7b61970a7a04b56dbf0
mdellabani <mdellabani@gmail.com>
2023-09-06 18:02:09 +0200

    issue number

--- core/src/main/java/io/squashql/query/database/QueryAwareQueryRewriter.java
+++ core/src/main/java/io/squashql/query/database/QueryAwareQueryRewriter.java
@@ -24,7 +24,7 @@ public class QueryAwareQueryRewriter implements QueryRewriter {
             && vt.name.equals(f.store())
             && vt.fields.contains(f.name())) {
       return SqlUtils.getFieldFullName(cteName(f.store()), fieldName(f.name()));
-      // todo-180 should we care about this case ?
+      // todo-181 should we care about this case ?
     } else {
       return this.underlying.getFieldFullName(f);
     }
