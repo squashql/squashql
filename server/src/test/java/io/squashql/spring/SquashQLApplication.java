b7fc22eb46bbe3bae42049ad96c0059e522a00d4
jacques-charnay <jch@hereforretail.io>
2023-10-16 10:46:18 +0200

    Create Jackson configuration

--- server/src/test/java/io/squashql/spring/SquashQLApplication.java
+++ server/src/test/java/io/squashql/spring/SquashQLApplication.java
@@ -1,10 +1,8 @@
 package io.squashql.spring;
 
-import io.squashql.jackson.JacksonUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -13,11 +11,6 @@ public class SquashQLApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(SquashQLApplication.class, args);
-  }
-
-  @Bean
-  public MappingJackson2HttpMessageConverter jackson2HttpMessageConverter() {
-    return new MappingJackson2HttpMessageConverter(JacksonUtil.OBJECT_MAPPER);
   }
 
   @Bean
