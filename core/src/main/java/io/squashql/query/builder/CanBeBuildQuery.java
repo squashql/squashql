8bb2981e16dbb0cbf37c6632934ee5b7810f7b11
paul <bares.paul@gmail.com>
2024-05-06 12:24:23 +0400

    revert changes

--- core/src/main/java/io/squashql/query/builder/CanBeBuildQuery.java
+++ core/src/main/java/io/squashql/query/builder/CanBeBuildQuery.java
@@ -1,11 +1,8 @@
 package io.squashql.query.builder;
 
-import io.squashql.query.dto.GroupColumnSetDto;
 import io.squashql.query.dto.QueryDto;
 
 public interface CanBeBuildQuery {
-
-  CanBeBuildQuery addGroupingSet(GroupColumnSetDto groupColumnSetDto);
 
   QueryDto build();
 }
--- core/src/main/java/io/squashql/query/builder/CanStartBuildingJoin.java
+++ core/src/main/java/io/squashql/query/builder/CanStartBuildingJoin.java
@@ -1,6 +1,5 @@
 package io.squashql.query.builder;
 
-import io.squashql.query.dto.GroupColumnSetDto;
 import io.squashql.query.dto.JoinType;
 import io.squashql.query.dto.VirtualTableDto;
 
@@ -9,6 +8,4 @@ public interface CanStartBuildingJoin {
   HasStartedBuildingJoin join(String tableName, JoinType joinType);
 
   HasStartedBuildingJoin join(VirtualTableDto virtualTableDto, JoinType joinType);
-
-  HasJoin join(GroupColumnSetDto groupColumnSetDto);
 }
